72c2c1adf64f637ca7617e3a5e19cde648d9e790
sialotobiko <sialo.tobiko@student.moringaschool.com>
2022-07-18 04:32:58 +0300

    This commit will make the second submit button responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,9 +37,29 @@ submit.addEventListener("click", showReport);
     location.reload();
 }*/
 
-/*function totalProduction() {
-    const shedA = parseInt(document.getElementById("sheda").value);
-    const shedB = parseInt(document.getElementById("shedb").value);
-    const shedC = parseInt(document.getElementById("shedc").value);
-    const shedD = parseInt(document.getElementById("shedd").value);
-} */
+function incomeOverTime(sellingPrice, time) {
+  sellingPrice = document.getElementById("sellp").value;
+  time = document.getElementById("timeperiod").value;
+
+  const shedA = parseInt(document.getElementById("sheda").value);
+  const shedB = parseInt(document.getElementById("shedb").value);
+  const shedC = parseInt(document.getElementById("shedc").value);
+  const shedD = parseInt(document.getElementById("shedd").value);
+  
+  const data = [shedA, shedB, shedC, shedD];
+  const sum = data.reduce((x, y) => x + y, 0);
+  var totalProd = parseInt(sum);
+  console.log(totalProd);
+
+ 
+
+  let weekly_income = totalProd * sellingPrice * (time * 7);
+  let yearly_income = totalProd * sellingPrice * (time * 365);
+  parseInt(weekly_income);
+  parseInt(yearly_income);
+
+  document.getElementById("weekly_income").innerHTML = "Your weekly income is " + weekly_income;
+  document.getElementById("yearly_income").innerHTML = "Your yearly income is " + yearly_income;
+
+}
+submit2.addEventListener("click", incomeOverTime);
